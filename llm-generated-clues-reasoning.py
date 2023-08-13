7429ff91c833afa67709c773efc2dfe9e0a42ba9
Auro Tripathy <ipserv@yahoo.com>
2023-08-13 14:15:38 -0700

    init

---
+++ llm-generated-clues-reasoning.py
@@ -0,0 +1,10 @@
+"""
+LLM generated CLUES and REASONING
+
+Using dataset/sign_events_data_statements.csv with columns: 
+signevent,q1,q2,unid,id,outcome_class
+
+Adding a CLUES and REASONING column
+"""
+
+
