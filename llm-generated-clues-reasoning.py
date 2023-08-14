77bf95fc797001d736acb0dab00b79a402c2d23a
Auro Tripathy <ipserv@yahoo.com>
2023-08-14 01:17:33 -0700

    WIP

--- llm-generated-clues-reasoning.py
+++ llm-generated-clues-reasoning.py
@@ -3,8 +3,96 @@ LLM generated CLUES and REASONING
 
 Using dataset/sign_events_data_statements.csv with columns: 
 signevent,q1,q2,unid,id,outcome_class
+Dataset size: [1640 rows x 6 columns]
 
-Adding a CLUES and REASONING column
+Adding the CLUES and REASONING column genersted by the LLM and
+calling it,
 """
 
 
+from openai_interface import init_openai, get_chat_completion_with_backoff
+from parse_output import parsed_response_str
+
+init_openai()
+
+dataset_used = 'dataset/sign_events_data_statements.csv'
+
+import pandas as pd
+df = pd.read_csv (dataset_used)
+# simple EDA
+print(df.columns)
+print(f'shape: {df.shape}')  # should be 1640 x 6
+print(df.head)
+
+newline = '\n'
+# reasoning_word_limit = 50
+# print the prelude
+def create_prelude(gt):
+    prelude = f"""
+In the PARAGRAPH below, 
+First, highlight in the JSON format, words or phrases related to the TRUTHFUL and DECEPTIVE categories.
+Treat each sub-category within the two categories in order of decreasing importance.
+Next, provide a detailed reason that the paragraph is truthful based on what you find in the sub-categories below.
+Finally, in one word, make a decisive classification on whether the paragraph is TRUTHFUL or DECEPTIVE. 
+TRUTHFUL
+ingestion - examples are: " dish", "eat", "pizza"
+biological-processes - examples are: "eat", "blood", "pain"
+numbers - examples are: "second", "thousand", "5", "10"
+leisure - examples are: "cook", "chat", "movie" 
+future-focus - examples are:  "may", "will", "soon"
+DECEPTIVE
+apostrophes - examples are, "haven't", "won't", "she's", "can't"
+past-tense-focused -  examples are, "ago", "did", "talked", "promised", "gotten"
+reward  - examples are, "congratulate", "accomplishment" , "take", "prize", "benefit"
+pronouns - examples are, "I", "them", "itself"
+personal-pronouns - examples are, "I", "them", "her"
+exclamation-mark - example is, "!"
+    """
+
+    return prelude + newline
+
+def create_input(row):
+    """
+    For now, conbine question 1 and question 2
+    """
+    return 'PARAGRAPH: ' + row['q1'] + '\n' + row['q2']
+
+
+def construct_context(row, gt):
+    """ 
+    put the prelude and the response to q1 and q2 
+    """
+    context = ''
+    # print(create_prelude())
+    context += create_prelude(gt)
+    context += create_input(df.loc[row].copy())
+    return context
+
+
+if __name__ == "__main__":
+    ground_truths = []
+    predictions = []
+    start_row, end_row = 1, 2
+    model = 'gpt-4'  # "gpt-3.5-turbo" or "gpt-4"
+    print(f'start row: {start_row} end row: {end_row}')
+    print(f'Model:{model}')
+    for row in range(start_row, end_row):
+
+        ground_truth = 'truthful' if df.loc[row]['outcome_class'] == 't' else 'deceptive'
+        final_context = construct_context(row=row, gt=ground_truth)
+        print(final_context)
+        # print(f'ground truth (GT): {ground_truth}')
+        # print(f'INPUT:\n Q1:\n {df.loc[row]["q1"]} \n Q2:\n {df.loc[row]["q2"]}')
+
+        response = get_chat_completion_with_backoff(final_context, model=model)
+        print(response + newline)
+        # dict_response = parsed_response_str(response)
+        # # print(dict_response)
+        # ground_truths.append(ground_truth)
+        # predictions.append(dict_response['CLASSIFICATION'].lstrip(' '))
+
+# for ground_truth, prediction in zip(ground_truths, predictions):
+#     print(f'GT: {ground_truth}, Pred: {prediction}')
+
+# from sklearn.metrics import f1_score
+# print('Weighted F1-score:', f1_score(ground_truths, predictions, average='weighted'))
