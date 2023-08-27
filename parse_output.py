f96030074d1c4fe419c5ffc804b386f1836fe005
Auro Tripathy <ipserv@yahoo.com>
2023-08-27 05:14:11 -0700

    less brittle parser for responses

--- parse_output.py
+++ parse_output.py
@@ -4,22 +4,28 @@ very brittle for now, need to fix
 """
 
 import re
-def parsed_response_str(response):
-    delims = ['CLUES:', '\nREASONING:', '\nCLASSIFICATION:']
-    delims_clean = ['CLUES', 'REASONING', 'CLASSIFICATION']
-    pattern = '|'.join(delims)
-    result = re.split(pattern, response)
-    result_list = list(filter(None, result))  # filter out the initial empty string
-    # print(result_list)
-    # print(delims)
-    if len(result_list) != len(delims):
-        print(len(result_list), len(delims))
-        exit('something went wrong')
-    else:
-        split_dict = {}
-        for delim, value in zip(delims_clean, result_list):
-            split_dict[delim] = value
-    return split_dict 
+import json
+
+# def parsed_response_str(response):
+#     delims = ['CLUES:', '\nREASONING:', '\nCLASSIFICATION:']
+#     delims_clean = ['CLUES', 'REASONING', 'CLASSIFICATION']
+#     pattern = '|'.join(delims)
+#     result = re.split(pattern, response)
+#     result_list = list(filter(None, result))  # filter out the initial empty string
+#     # print(result_list)
+#     # print(delims)
+#     if len(result_list) != len(delims):
+#         print(len(result_list), len(delims))
+#         exit('something went wrong')
+#     else:
+#         split_dict = {}
+#         for delim, value in zip(delims_clean, result_list):
+#             split_dict[delim] = value
+#     return split_dict 
+
+def extract_classification(response):
+    response_array = response.split('\n')
+    return json.loads(response_array[2])['classification']
 
 
 if __name__ == "__main__":
