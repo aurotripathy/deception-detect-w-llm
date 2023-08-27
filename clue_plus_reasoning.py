0861160c170b9b30d54b4074b874d716218bc808
Auro Tripathy <ipserv@yahoo.com>
2023-08-27 06:00:40 -0700

    conversion from dict to json

--- clue_plus_reasoning.py
+++ clue_plus_reasoning.py
@@ -1,4 +1,9 @@
 """
+k-shot learning on how to classify thuthfuil adn deceptive statements 
+based on hand-written (now llm generated) clues and reasoning
+"""
+
+"""
 Inspiraton from: 
 Text Classification via Large Language Models
 Xiaofei Sun, Xiaoya Li, Jiwei Li, Fei Wu, Shangwei Guo, Tianwei Zhang, Guoyin Wang
@@ -32,12 +37,15 @@ sentiment:
 """
 
 from openai_interface import init_openai, get_chat_completion_with_backoff
-from parse_output import parsed_response_str
+from parse_output import extract_classification
+import json
 
 init_openai()
 
 import pandas as pd
-df = pd.read_csv ('dataset/dataset_plus_filtered_liwc.csv')
+# df = pd.read_csv ('dataset/dataset_plus_filtered_liwc.csv')
+df = pd.read_csv ('dataset/llm_generated_clues_reasoning_events_data_statements.csv')
+
 # simple EDA
 # print(df)
 # print(df.columns)
@@ -46,7 +54,7 @@ print(f'shape: {df.shape}')  # should be 1640 x 6
 
 count = 0
 for index, row in df.iterrows():
-    if row['has_clues']==1:
+    if row['contains_clues'] == 1:
         count += 1
         print(f"[{row['id'] + 1}], {row['clues']}, {row['reasoning']}")
 print(f'Count: {count}')
@@ -68,7 +76,7 @@ def create_prelude():
 
 def create_input(row):
     """
-    For now conbine question1 and question 2
+    For now, combine question 1 and question 2 into a single input
     """
     return 'INPUT: ' + row['q1'] + '\n' + row['q2']
 
@@ -76,70 +84,80 @@ def create_clues(row):
     """
     use the style from the paper
     """
-    return 'CLUES: ' + row['clues']
+    # return json.dumps({'CLUES' : json.dumps(row['clues'])})
+    return json.dumps(json.loads(row['clues']))
 
 def create_reasoning(row):
-    return 'REASONING' + ': ' + row['reasoning']
+    return json.dumps(json.loads(row['reasoning']))
 
-def setup_classification(row):
-    return('CLASSIFICATION: ' + ('truthful' if row['outcome_class'] == 't' else 'deceptive'))
+def create_classification(row):
+    # return dict({'CLASSIFICATION': 'truthful' if row['outcome_class'] == 't' else 'deceptive'})
+    return json.dumps(json.loads(row['classification']))
 
 def create_one_of_k_shots(row):
     k_shot = ''
     k_shot += create_input(row) + newline
-    k_shot += create_clues(row) + newline
-    k_shot += create_reasoning(row) + newline
-    k_shot += setup_classification(row) + newline
+    k_shot += str(create_clues(row)) + newline
+    k_shot += str(create_reasoning(row)) + newline
+    k_shot += str(create_classification(row)) + newline
     return k_shot
 
 
 def setup_inference(row):
-    """ we leave CLUEs, REASINING, and CLASSIFICATION blank for the LLM to fill up"""
+    """ we leave CLUEs, REASONING, and CLASSIFICATION blank for the LLM to generate"""
     inference = ''
     inference += create_input(row) + newline
-    inference += 'CLUES:' + newline
-    inference += 'REASONING: ' + newline
-    inference += 'CLASSIFICATION:' + newline
+    inference += str(json.dumps(json.loads('{"CLUES" : ""}'))) + newline
+    inference += str(json.dumps(json.loads('{"REASONING": ""}'))) + newline
+    inference += str(json.dumps(json.loads('{"classification": ""}'))) + newline
     return inference
 
 
-def construct_context(inference_row):
-    """ constructs the k-shots and the section that has to inferred"""
+def construct_context(inference_row, k_shot_count):
+    """ constructs the k-shots and the section that has to inferred
+        The k is determined by the has_clues flag, this will change...
+    """
     context = ''
     # print(create_prelude())
     context += create_prelude()
+    count = 0
     for index, row in df.iterrows():
-        if row['has_clues'] == 1:
+        if row['contains_clues'] == 1:
             # print(row['clues'], row['reasoning'])
             # create_one_of_k_shots(row)
             context += create_one_of_k_shots(row)
+            count += 1
             # print('\n')
             context += newline
-    
+        if count == k_shot_count:
+            break
+
     context += setup_inference(df.loc[inference_row].copy())
     return context
 
 
-ground_truths = []
-predictions = []
 if __name__ == "__main__":
-    start_row, end_row = 900, 910
+    k_shot_count = 3
+    ground_truths = []
+    predictions = []
+    start_row, end_row = 900, 920
     model = 'gpt-4'  # "gpt-3.5-turbo" or "gpt-4"
     print(f'start row: {start_row} end row: {end_row}')
     print(f'Model:{model}')
     for row in range(start_row, end_row):
-
-        final_context = construct_context(inference_row=row)
+        print(f"{20*'-'}{row} context GT {20*'-'}")
+        final_context = construct_context(row, k_shot_count)
+        print(final_context)
         ground_truth = 'truthful' if df.loc[row]['outcome_class'] == 't' else 'deceptive'
         print(f'ground truth (GT): {ground_truth}')
-        print(f'INPUT:\n Q1:\n {df.loc[row]["q1"]} \n Q2:\n {df.loc[row]["q2"]}')
 
         response = get_chat_completion_with_backoff(final_context, model=model)
+        print(f"{20*'>'}{row} response GT {20*'>'}")
         print(response + newline)
-        dict_response = parsed_response_str(response)
-        # print(dict_response)
+        
+        predicted_class = extract_classification(response)
         ground_truths.append(ground_truth)
-        predictions.append(dict_response['CLASSIFICATION'].lstrip(' '))
+        predictions.append(predicted_class)
 
 for ground_truth, prediction in zip(ground_truths, predictions):
     print(f'GT: {ground_truth}, Pred: {prediction}')
